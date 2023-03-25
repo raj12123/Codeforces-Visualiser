6b8b2a97f915cba31579c0dfa040c864f16cdf5c
rajaryan12123 <rajaryan12123@gmail.com>
2023-03-26 02:31:27 +0530

    first commit

---
+++ Codeforces Visualiser/Codeforces_VisualiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Codeforces_VisualiserApp.swift
+//  Codeforces Visualiser
+//
+//  Created by Raj Aryan on 26/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Codeforces_VisualiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
